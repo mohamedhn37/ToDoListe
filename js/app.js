bff43def022b3d217dadb2e0b565149287a74c10
Mohamed Hannaoui <hannaoui20.mohamed@gmail.com>
2023-03-13 10:36:56 +0100

    somechage

--- js/app.js
+++ js/app.js
@@ -3,12 +3,16 @@ $(document).ready(function () {
         let input = $(this).val();
         $('ul').append('<li>'+ input +'<i class="fa-solid fa-check"></i><i class="fa-solid fa-trash"></i></li>');
         $(this).val('');
-        $('ul').on('click','.fa-trash', function () {
-            $(this).parent('li').fadeOut(200);
+        $('ul').on('click', '.fa-trash', function() {
+            $(this).parent().fadeOut(200, function() {
+            $(this).remove();
+            });
         });
-        $('ul').on('click','.fa-check', function () {
-            $(this).parent('li').toggleClass('checked');
+            
+        $('ul').on('click', '.fa-check', function() {
+            $(this).parent().toggleClass('checked');
         });
     });
 });
 
+ 
